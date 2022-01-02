238e3f40880cba7aab7a31c40c34e992108e9e84
TomL <youtom.lee17@gmail.com>
2022-01-02 10:08:27 +0800

    Set up node server serving 4 simple pages

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    const baseURL = 'http://' + req.headers.host;
+    const q = new URL(req.url, baseURL);
+    //  console.log(q);
+
+    const fileName = (q.pathname === '/') ? 'index.html' : `.${q.pathname}.html`;
+    // console.log(fileName);
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            //     console.log(err);
+            fs.readFile('404.html', (err404, data404) => {
+                if (err404) throw err404;
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.write(data404);
+                return res.end();
+            })
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            return res.end();
+        }
+    })
+}).listen(8080, () => console.log('Listening on port 8080...'));
